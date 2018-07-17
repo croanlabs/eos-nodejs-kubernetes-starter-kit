58d72d252f8a8dcc295b87869014c1f4419fbd54
etb <etb@etbs-MacBook-Pro.local>
2018-07-17 14:17:01 +0100

    Update clean script after changing the node.yaml file

--- clean.sh
+++ clean.sh
@@ -1,5 +1,3 @@
-kubectl delete service nodejs-master
-kubectl delete deployment nodejs-deployment
 kubectl delete service eos-master
 kubectl delete deployment eos-deployment
 kubectl delete pod nodejs-pod
